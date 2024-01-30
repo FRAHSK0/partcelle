30d5d5b6285d2e5f4eeb3525d1ac2212b13079ab
FRAHSK0 <126199177+FRAHSK0@users.noreply.github.com>
2024-01-30 23:41:45 +0100

    parte 2.0

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +17,16 @@ namespace partcelle
             n = 100;
          List< Vettore > list = new List<Vettore> ();
          Random rx = new Random ();
-        
-            for (int i = 0; i < n; i++)
+         using (StreamWriter sw = new StreamWriter("c:\\Users\\Francesco\\Desktop\\P.xslm")){
+                for (int i = 0; i <n; i++)
             {
                 
                 Vettore v= new Vettore(rx.Next(3,50),rx.Next(3,50)); 
                 list.Add(v);
             }
-            int x2;
-            int y2;
+           
             
-            for (int i = 0;i < 30; i++)
+            for (int i = 0;i <31; i++)
             {
             foreach ( Vettore Z in list) 
             {
@@ -35,22 +35,21 @@ namespace partcelle
                     Z.X =Z.X+rx.Next(1,2);
                     Z.Y= Z.X+rx.Next(1,2);
             }
-               
-            }
-            int c;
+             int c;
             c = 0;
 
 
             foreach (Vettore Z in list)
             {
-                if ((Z.X < 51) && (Z.Y < 51))
-                {
+                if ((Z.X<51)&&(Z.Y<51)){
                     c++;
                 }
             }
-            Console.WriteLine("ci sono particelle  {0}", c);
+            Console.WriteLine("ci sono al tempo {0}  particelle  {1}", i,c);
+            sw.WriteLine($"{i};tempo;numero particelle;{c};");
+            }
 
-
+            }
             Console.ReadLine();
         }
     }
